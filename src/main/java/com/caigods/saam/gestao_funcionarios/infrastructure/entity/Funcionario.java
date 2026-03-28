f5dfea593386920b62f7d38fac021bf57d9e2171
Caigods <caique.godinho337@outlook.com>
2026-03-27 22:16:41 -0300

    Initial commit

---
+++ src/main/java/com/caigods/saam/gestao_funcionarios/infrastructure/entity/Funcionario.java
@@ -0,0 +1,9 @@
+package com.caigods.saam.gestao_funcionarios.infrastructure.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name= "")
+public class Funcionario {
+}
---
+++ src/test/java/com/caigods/saam/gestao_funcionarios/GestaoFuncionariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caigods.saam.gestao_funcionarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoFuncionariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
